e9e8d860c579abb90feb8a811715203739f20945
unknown <mayank757511@gmail.com>
2025-01-05 11:11:09 +0530

    Bug fixed

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -61,7 +61,7 @@ function Login() {
               </button>
             </form>
             <p className="mt-4 text-center text-sm text-gray-600">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <Link to="/register" className="text-blue-600 hover:underline">
                 Register here
               </Link>
